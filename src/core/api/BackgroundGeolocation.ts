70c1bb896458aefbb3c3e6803185009e901f34c8
Chris Scott <christocracy@gmail.com>
2026-02-10 11:10:20 -0500

    Fix indenting

--- src/core/api/BackgroundGeolocation.ts
+++ src/core/api/BackgroundGeolocation.ts
@@ -547,7 +547,7 @@ export interface BackgroundGeolocationAPI extends BackgroundGeolocationEvents {
   readonly logger: Logger;
 
   /**
-  *<!-- doc-id: BackgroundGeolocation.ready -->
+  * <!-- doc-id: BackgroundGeolocation.ready -->
   *
   * Signal to the plugin that your app is launched and ready, proving the default {@link Config}.
   *
